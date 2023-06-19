786b9203ef9f0fa694909edea6823cf780771e53
Tornike Shelia <tornikeshelia1996@gmail.com>
2023-06-19 17:10:08 +0400

    Initial commit

---
+++ src/main/java/com/acme/test01/tornikeshelia/TornikesheliaApplication.java
@@ -0,0 +1,13 @@
+package com.acme.test01.tornikeshelia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TornikesheliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TornikesheliaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acme/test01/tornikeshelia/TornikesheliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.test01.tornikeshelia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TornikesheliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
